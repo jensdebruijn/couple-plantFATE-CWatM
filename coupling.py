230e2650decb0c8f25df44d8439b323112290f45
Jens de Bruijn <j.a.debruijn@outlook.com>
2023-04-17 11:38:11 +0200

    Update following email Jaideep

--- coupling.py
+++ coupling.py
@@ -1,44 +1,63 @@
 def run_plantFATE(soil_water_potential_CWatM):
     return psi
 
-def couple_plantFATE(
-        soil_moisture_layer_1,
-        soil_moisture_layer_2,
-        soil_moisture_layer_3,
-        soil_tickness_layer_1,
-        soil_tickness_layer_2,
-        soil_tickness_layer_3,
-        precipitation,
-        water_pressure_deficit,
-        irradiance,
-        irrigation=0, # forest never has has irrigation
-    ):
+def calculate_soil_water_potential(soil_moisture, soil_thickness):
+    # TODO: Implement
+    return soil_water_potential
     
-    assert irigation == 0
+def calculate_vapour_pressure_deficit(temperature, relative_humidity):
+    # TODO: Implement
+    return vapour_pressure_deficit
     
-    soil_water_potential_CWatM = xxxx # check with Mikhail
+def calculate_photosynthetically_active_radiation(shortwave_radiation, longwave_radiation):
+    # TODO: Implement
+    return photosynthetically_active_radiation
 
-    psi = run_plantFATE(
-        soil_water_potential_CWatM,
-        vapour pressure deficit,
-        photosynthetic active radiation,
+def couple_plantFATE(
+        soil_moisture_layer_1,  # ratio [0-1]
+        soil_moisture_layer_2,  # ratio [0-1]
+        soil_moisture_layer_3,  # ratio [0-1]
+        soil_tickness_layer_1,  # m
+        soil_tickness_layer_2,  # m
+        soil_tickness_layer_3,  # m
+        temperature,  # degrees Kelvin
+        relative_humidity,  # ratio [0-1]
+        shortwave_radiation,  # W/m2
+        longwave_radiation  # W/m2
+    ):
+        
+    soil_water_potential_1 = calculate_soil_water_potential(soil_moisture_layer_1, soil_tickness_layer_1)
+    soil_water_potential_2 = calculate_soil_water_potential(soil_moisture_layer_2, soil_tickness_layer_2)
+    soil_water_potential_3 = calculate_soil_water_potential(soil_moisture_layer_3, soil_tickness_layer_3)
+    
+    vapour_pressure_deficit = calculate_vapour_pressure_deficit(temperature, relative_humidity)
+    
+    photosynthetically_active_radiation = calculate_photosynthetically_active_radiation(shortwave_radiation, longwave_radiation)
+
+    evapotranspiration = run_plantFATE(
+        [
+            soil_water_potential_1,
+            soil_water_potential_2,
+            soil_water_potential_3
+        ],
+        vapour_pressure_deficit,
+        photosynthetically_active_radiation,
         temperature
     )
     
-    evapotranspiration = # how do I calculate this?
     return evapotranspiration
 
 
 if __name__ == '__main__':
     couple_plantFATE(
-        soil_moisture_layer_1=0.7, # ratio [0-1]
-        soil_moisture_layer_2=0.5, # ratio [0-1]
-        soil_moisture_layer_3=0.3, # ratio [0-1]
-        soil_tickness_layer_1=0.05, # meter
-        soil_tickness_layer_2=0.3, # meter
-        soil_tickness_layer_3=2, # meter
-        precipitation=1, # mm
-        water_pressure_deficit=0 # check with Mikhail
-        irradiance=0.5, # check with Mikhail
-        irrigation=0, # mm, forest never has has irrigation
+        soil_moisture_layer_1=0.7,  # ratio [0-1]
+        soil_moisture_layer_2=0.5,  # ratio [0-1]
+        soil_moisture_layer_3=0.3,  # ratio [0-1]
+        soil_tickness_layer_1=0.05,  # m
+        soil_tickness_layer_2=0.5,  # m
+        soil_tickness_layer_3=3,  # m
+        temperature=293,  # degrees Kelvin
+        relative_humidity=0.70,  # ratio [0-1]
+        shortwave_radiation,  # W/m2
+        longwave_radiation  # W/m2
     )
