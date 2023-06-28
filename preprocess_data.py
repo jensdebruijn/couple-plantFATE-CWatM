1e68e5935cf3937ed894368d3c854092e010b175
Elisa Stefaniak <elisa@foxeswithdata.com>
2023-06-28 14:49:00 +0200

    Continuing work on preprocessing test 'model input

--- preprocess_data.py
+++ preprocess_data.py
@@ -5,6 +5,7 @@
 
 import datetime
 import pandas
+import math
 
 # First we want to extrapolate the met data so that it is on a daily timescale and not a monthly one.
 # This is going to be very, very simple and only copying previous data
@@ -16,13 +17,13 @@ indx_last = df_met.tail(1).index[0]
 date_first = datetime.date(df_met.Year[0], df_met.Month[0], 1)
 date_last = datetime.date(df_met.Year[indx_last], df_met.Month[indx_last], 31)
 
+i = 0
+out = []
+while i <= df_met.last_valid_index():
+    out.append(datetime.date(df_met.Year[i], df_met.Month[i], 1).isoformat())
+    i += 1
 
-def makedate(row):
-    return datetime.date(row['Year'], row['Month'], 1)
-
-
-df_met['date'] = df_met.apply(lambda df: makedate(df), axis=1)
-
+df_met['date'] = out
 dates_met = []
 delta_date = datetime.timedelta(days=1)
 start_dt = date_first
@@ -33,13 +34,30 @@ while start_dt <= end_dt:
     start_dt += delta_date
 
 dates_met_df = {'date': dates_met}
-
+dates_met_df = pandas.DataFrame(dates_met, columns=['date'])
 df_met_full = pandas.merge(df_met, dates_met_df, how="right", on="date")
+
+i = 1
+
+while i <= df_met_full.last_valid_index():
+    if math.isnan(df_met_full.Temp[i]):
+        df_met_full.Year[i] = df_met_full.Year[i-1]
+        df_met_full.Month[i] = df_met_full.Month[i - 1]
+        df_met_full.Temp[i] = df_met_full.Temp[i - 1]
+        df_met_full.VPD[i] = df_met_full.VPD[i - 1]
+        df_met_full.PAR[i] = df_met_full.PAR[i - 1]
+        df_met_full.PAR_max[i] = df_met_full.PAR_max[i - 1]
+        df_met_full.SWP[i] = df_met_full.SWP[i - 1]
+    i += 1
+
+
+
 
 # Just to play around let's examine the cwatm input
 
 df_water = pandas.read_csv('plantFATE.csv')
 print(df_water.head(1))
+
 
 # So we basically would like to take 1 year from cwatm and 1 year from plantFATE to start off with
 # so that we have the same dates in both scenarios
