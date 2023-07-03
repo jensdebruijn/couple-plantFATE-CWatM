e50bd327665bbf7c844d93aa9ae0ebe9ac269f0a
Elisa Stefaniak <elisa@foxeswithdata.com>
2023-07-03 15:14:12 +0200

    Preparing dataset for reading

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,4 +1,3 @@
-# todo: extrapolate met data to daily scale
 # todo: convert parameters to daily scale
 # todo: convert variables to daily scale as well
 # todo: make sure to have all parameters/vars in a table with units defined
@@ -19,7 +18,7 @@ date_last = datetime.date(df_met.Year[indx_last], df_met.Month[indx_last], 31)
 
 i = 0
 out = []
-while i <= df_met.last_valid_index():
+while i < len(df_met):
     out.append(datetime.date(df_met.Year[i], df_met.Month[i], 1).isoformat())
     i += 1
 
@@ -33,37 +32,30 @@ while start_dt <= end_dt:
     dates_met.append(start_dt.isoformat())
     start_dt += delta_date
 
-dates_met_df = {'date': dates_met}
 dates_met_df = pandas.DataFrame(dates_met, columns=['date'])
 df_met_full = pandas.merge(df_met, dates_met_df, how="right", on="date")
 
 i = 1
 
-while i <= df_met_full.last_valid_index():
+while i < len(df_met_full):
     if math.isnan(df_met_full.Temp[i]):
-        df_met_full.Year[i] = df_met_full.Year[i-1]
-        df_met_full.Month[i] = df_met_full.Month[i - 1]
-        df_met_full.Temp[i] = df_met_full.Temp[i - 1]
-        df_met_full.VPD[i] = df_met_full.VPD[i - 1]
-        df_met_full.PAR[i] = df_met_full.PAR[i - 1]
-        df_met_full.PAR_max[i] = df_met_full.PAR_max[i - 1]
-        df_met_full.SWP[i] = df_met_full.SWP[i - 1]
+        df_met_full.loc[i, 'Year'] = df_met_full.Year[i - 1]
+        df_met_full.loc[i, 'Month'] = df_met_full.Month[i - 1]
+        df_met_full.loc[i, 'Temp'] = df_met_full.Temp[i - 1]
+        df_met_full.loc[i, 'VPD'] = df_met_full.VPD[i - 1]
+        df_met_full.loc[i, 'PAR'] = df_met_full.PAR[i - 1]
+        df_met_full.loc[i, 'PAR_max'] = df_met_full.PAR_max[i - 1]
+        df_met_full.loc[i, 'SWP'] = df_met_full.SWP[i - 1]
     i += 1
-
-
-
 
 # Just to play around let's examine the cwatm input
 
 df_water = pandas.read_csv('plantFATE.csv')
 print(df_water.head(1))
 
+## OK and let's merge the two dataframes
 
-# So we basically would like to take 1 year from cwatm and 1 year from plantFATE to start off with
-# so that we have the same dates in both scenarios
+df_full = pandas.merge(df_met_full, df_water, how="inner", on="date")
 
-indx_last = df_water.tail(1).index[0]
-date_first_cwatm = datetime.datetime.strptime(df_water.date[0], '%Y-%m-%d').date()
-date_last_cwatm = datetime.datetime.strptime(df_water.date[indx_last], '%Y-%m-%d').date()
+df_full.loc[:, 'Temp'] = df_full.Tavg
 
-df_water['date_obj'] = df_water['date'].map(lambda dt: datetime.datetime.strptime(dt, '%Y-%m-%d').date())
